7b9bc39d6e91f5514b7bbcde72292180e01a85f3
GuilhermeBialas <38558633+GuilhermeBialas@users.noreply.github.com>
2018-05-22 00:13:06 -0300

    inicio do cadastroServico

---
+++ JavaApplication1/src/CadastroServico.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Guilherme Bialas
+ */
+public class CadastroServico {
+    String[] nomeProdutos = new String[100];
+    String[] descricao = new String[100];
+    double[] valorPago = new double[100];
+    String[] abrangecia = new String[100];
+    int[] tempoMaxEntrega = new int[100];
+    double[] pesoMaxCorresp = new double[100];
+    String[] tributacao = new String[100];
+    
+}
+
+public void Cadastro() {
+
+}
