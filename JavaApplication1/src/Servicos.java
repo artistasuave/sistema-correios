f6e0603a1300a8f73a738fe46294d1972c624ec7
GuilhermeBialas <38558633+GuilhermeBialas@users.noreply.github.com>
2018-05-24 10:37:00 -0300

    atualização de Serviços

--- JavaApplication1/src/Servicos.java
+++ JavaApplication1/src/Servicos.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,8 +13,117 @@
  */
 public class Servicos {
     
-    String[] nome = new String[100];
+    public void menuServicos(){
+        int menuDeServicos = Integer.parseInt(JOptionPane.showInputDialog(null, 
+                "\n1 - Cadastrar o serviço"
+                +"\n2 - Buscar pelo serviço"
+                +"\n3 - Editar"
+                +"\n8001 - SAIR"));
+   
+    while(menuDeServicos != 8001){
+        
+        switch (menuDeServicos){
+            case 1:
+                cadastrar();
+                break;
+            case 2:
+                buscarPeloServico();
+                break;
+            case 3:
+                editar();
+            default:
+                    JOptionPane.showMessageDialog(null, "Opção invalida");
+        }
+           menuDeServicos = Integer.parseInt(JOptionPane.showInputDialog(null,
+                     "1 - Cadastrar"
+                    + "\n2 - Buscar"
+                    + "\n3 - Editar"
+                    + "\n8001 - Sair"));
+                   
+     
+    }
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    String[] nomes = new String[100];
     String[] descricao = new String[100];
     double[] valorPago = new double[100];
+    String[] abrangencia = new String[100];
+    int[] tempoMaxEntrega = new int[100];
+    double[] pesoMaxCorrespodencia = new double[100];
+    String[] tributacao = new String[100];
+    int atual = 0;
+    
+    public void cadastrar(){
+        solicitarInformacao(atual);
+        atual++;
+    }
+    public void editar(){
+        String busca = JOptionPane.showInputDialog("Digite o nome do produto para editar");
+          for(int posicao = 0; posicao < atual; posicao++){
+              if(nomes[posicao].equals(busca)){
+                  solicitarInformacao(posicao);
+                  return;
+              }
+          }
+   
+    }
+public void listar(){
+    String texto = "";
+    for(int posicao = 0; posicao < atual; posicao++){
+        texto += nomes[posicao] + " "+ valorPago[posicao]+ "\n";
+}
+   JOptionPane.showMessageDialog(null, texto);
+}
+public void contabilizarPeloServico(){
+    String busca = JOptionPane.showInputDialog("Digite o nome do  serviço");
+    int quantidadeRegistros = 0;
+    for(int posicao; posicao < atual; atual++){
+        if(nomes[posicao].contains(busca)){
+            quantidadeRegistros++;
+        }
+    }
+ JOptionPane.showMessageDialog(null,  "A quantidade de "+ busca+ " é "+ quantidadeRegistros);
+ 
+}
+public void buscarPeloServico(){
+    String busca = JOptionPane.showInputDialog("Digite o derviço para efetuar a busca");
+    for(int posicao = 0; posicao < atual; posicao++){
+        if(nomes[posicao].contains(busca)){
+            apresentarInformacao(posicao);
+        }
+    }
+}
+public void apresentarInformacao(int posicao){
+    JOptionPane.showMessageDialog(null, 
+            "\nServiço: "+ nomes[posicao]
+            +"\nDescrição: "+ descricao[posicao]
+            +"\nValor pago: "+ valorPago[posicao]+ " reais"
+            +"\nÁrea de abrangência: " + abrangencia[posicao]
+            +"\nTempo máximo para a entrega:"+ tempoMaxEntrega[posicao]+ " dias"
+            +"\nPeso máximo da correspodência: "+ pesoMaxCorrespodencia[posicao]+ " Kg"
+            +"\nTributação: "+ tributacao[posicao]+ " reais");
+}
+public void solicitarInformacao(int posicao){
+    nomes[posicao] = JOptionPane.showInputDialog("Digite o nome do serviço").trim();
+    descricao[posicao] = JOptionPane.showInputDialog("Descrição do serviço");
+    valorPago[posicao] = Integer.parseInt(JOptionPane.showInputDialog("valor do serviço"));
+    abrangencia[posicao] = JOptionPane.showInputDialog("Abrangência");
+    tempoMaxEntrega[posicao] = Integer.parseInt(JOptionPane.showInputDialog("Tempo máximo da entrega"));
+    pesoMaxCorrespodencia[posicao] = Double.parseDouble(JOptionPane.showInputDialog("Peso máximo da correspodêcia"));
+    tributacao[posicao] = JOptionPane.showInputDialog("Tributação do serviço");
+    
+    
+}
     
 }
